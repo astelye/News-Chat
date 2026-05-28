acb2c49e34918158deef4d027941e0ac402b247c
Diego <88157099+Astelee@users.noreply.github.com>
2026-05-28 17:53:08 -0300

    Atualizar o AppNavigator.tsx

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -5,6 +5,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { LoginScreen } from '../screens/LoginScreen';
 import { RegisterScreen } from '../screens/RegisterScreen';
 import { HomeScreen } from '../screens/HomeScreen';
+import { ChatScreen } from '../screens/ChatScreen';    // Nova tela
+import { RankingScreen } from '../screens/RankingScreen'; // Nova tela
 
 const Stack = createNativeStackNavigator();
 
@@ -21,6 +23,8 @@ export const AppNavigator = () => {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+        <Stack.Screen name="Ranking" component={RankingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
